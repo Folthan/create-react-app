b5aca2c5b4f966368cf6753f72b6867295f164b3
Bianca van der Werf <bianca@create.nl>
2018-08-02 20:27:15 +0200

    Add file imports to react

---
+++ react-shop/src/assets/js/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>The end</p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ react-shop/src/assets/js/components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Navigation extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Shop</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navigation;
